117e9522f50ff4305d78729ba0409c1bd00461f7
yash <yashreverse@gmail.com>
2024-05-31 11:43:49 +0530

    started creating Input.java to generate inputs for my problems

---
+++ Input.java
@@ -0,0 +1,16 @@
+package javacode;
+
+import java.util.Random;
+
+public class Input {
+
+    private static final Random random = new Random();
+
+    public static int[] getSingleDimensionArray(int size, int start, int end) {
+        int[] array = new int[size];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(start, end);
+        }
+        return array;
+    }
+}
