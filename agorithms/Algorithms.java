fae36282ac199952389494da58318a58dd2f5c3f
yash <yashreverse@gmail.com>
2024-06-04 09:58:42 +0530

    solved the rotate string check substring problem

--- agorithms/Algorithms.java
+++ agorithms/Algorithms.java
@@ -46,6 +46,12 @@ public class Algorithms {
         System.out.println("no matches were found");
     }
 
+    /*796. Rotate String
+      Given two strings s and goal, return true if and only if s can become goal after some number of shifts on s.
+      A shift on s consists of moving the leftmost character of s to the rightmost position.
+      For example, if s = "abcde", then it will be "bcdea" after one shift.
+    */
+
     public static void rotateStringEquals(String input, String goal) {
 
         boolean same = false;
